6d765f79eb727822759f7eb16bc6a1df08e702a1
Karl Kuusik <karl32123@gmail.com>
2019-10-29 23:33:40 +0200

    Added editmode activation to admin page

---
+++ admin/admin_api.php
@@ -0,0 +1,21 @@
+<?php 
+$response = "";
+
+if(!empty($_POST["edit_key"])){
+    $conn = new PDO('mysql:host=localhost;dbname=userdata', 'root', 'Kilud123');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $query = $conn->prepare('SELECT * FROM editkeys WHERE keyname = ?');
+    $query->execute(array($_POST["edit_key"]));
+    $data = $query -> fetchAll();
+	foreach($data as $row){
+        $response .= $row["hashCode"];
+        break;
+    }
+    
+    http_response_code(200);
+    echo $response;
+}else{
+    http_response_code(403);
+    echo "Vigane päring!";
+}
+?>
--- admin/index.php
+++ admin/index.php
@@ -28,101 +28,100 @@
 
 ?>
 <html>
-	<head>
-		<title>praktika.ut.ee - Administrator</title>
-		<meta charset="utf-8">
-    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    	<link href="../css/creative.min.css" rel="stylesheet">
-    	<link rel="stylesheet" href="../js/trumbowyg/ui/trumbowyg.min.css">
-		<style>
-			.trumbowyg-editor,
-			.trumbowyg-box{ 
-				min-height: 100px !important;
-			}
-		</style>
-	</head>
-	<body>
-		
-		<div class="container">
-			<div class="row">
-				
-				<div class="col-md-12 my-5">
-					<h1>Tekstide muutmiseks sisestage alla lahtrisse salavõti.</p>
-					<input type="text" name="key" class="admin-key-input">
-				</div>
 
-				<div class="col-md-12 my-5">
-					<h2>Üliõpilase vaate tekstid</h2>
-					<form class="container work" target="_self" method="post" enctype="multipart/form-data">
-						<div class="row">
-							<p>
-								<textarea class="form-control" id="area"></textarea>
-							</p>
-						</div>
-					</form>
-				</div>
+<head>
+    <title>praktika.ut.ee - Administrator</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="../css/creative.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../js/trumbowyg/ui/trumbowyg.min.css">
+    <style>
+        .trumbowyg-editor,
+        .trumbowyg-box {
+            min-height: 100px !important;
+        }
 
+    </style>
+</head>
 
+<body>
 
-				<div class="col-md-12 my-5">
-					<h2>Organisatsiooni vaate tekstid</h2>
-				</div>
+    <div class="container">
+        <div class="row">
 
+            <div class="col-md-12 my-5">
+                <h3>Sisesta võti ja vajuta nuppu muutmisreziimi aktiveerimiseks</h3>
+                <input type="text" name="key" class="admin-key-input">
+                <div class="btn-group btn-group-md align-self-center" id="editmodeActivator">
+                    <span class="btn btn-sm btn-success">Aktiveeri muutimisreziim</span>
+                </div>
+            </div>
+            <div class="col-md-12 my-5">
+                <h2>Üleslaetud projektid koos nende muutmiseks vajalike linkidega</h2>
+                <div class="container">
+                    <div class="row">
+                        <?php 
+                            foreach ($projects as $p) {
+                                $bigString = '<div class="col-md-12">
+                                    <div class="card">
+                                        <div class="card-body text-left">
+                                            <h6 class="card-title text-uppercase font-weight-bold mt-0">'.$p["title"].'</h6>
+                                            <p class="card-text">Loodud: '.$p["start_date"].'<br> Reg. lõpp: '.$p["end_date"].'<br></p>
+                                            <div class="btn-group btn-group-md align-self-center" role="group" aria-label="Basic example">
+                                                <a class="btn btn-sm btn-success" href="../team/viewproject?c='.$p["id"].'&e='.$p["edit_key"].'">Mine muutma</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>';
+                                echo $bigString;
+                            }
+                        ?>
+                    </div>
+                </div>
+            </div>
 
-				<div class="col-md-12 my-5">
-					<h2>Projektide vaate tekstid</h2>
-				</div>
-				<div class="col-md-12 my-5">
-					<h2>Üleslaetud projektid koos nende muutmiseks vajalike linkidega</h2>
-					<div class="container">
-						<div class="row">
+        </div>
+    </div>
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-							<?php 
-	                            foreach ($projects as $p) {
-	                                $bigString = '<div class="col-md-12">
-	                                    <div class="card">
-	                                        <div class="card-body text-left">
-	                                            <h6 class="card-title text-uppercase font-weight-bold mt-0">'.$p["title"].'</h6>
-	                                            <p class="card-text">Loodud: '.$p["start_date"].'<br> Reg. lõpp: '.$p["end_date"].'<br></p>
-	                                            <div class="btn-group btn-group-md align-self-center" role="group" aria-label="Basic example">
-													<a class="btn btn-sm btn-success" href="../team/viewproject?c='.$p["id"].'&e='.$p["edit_key"].'">Mine muutma</a>
-												</div>
-	                                        </div>
-	                                    </div>
-	                                </div>';
-	                                echo $bigString;
-	                            }
-	                        ?>
-							
-						</div>
-					</div>
-				</div>
+    <!-- Plugin JavaScript -->
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="../vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
-			</div>
-		</div>
-		<script src="../vendor/jquery/jquery.min.js"></script>
-	    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Custom scripts for this template -->
+    <script src="../js/creative.min.js"></script>
+    <script src="../js/trumbowyg/trumbowyg.min.js"></script>
+    <script type="text/javascript">
+        $('#area').trumbowyg({
+            autogrow: true
+        });
 
-	    <!-- Plugin JavaScript -->
-	    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
-	    <script src="../vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+        ('.trumbowyg-button-pane').css('display', 'none');
+        $('.trumbowyg-box').focusout(function(event) {
+            $(this).find('.trumbowyg-button-pane').fadeOut(200);
+        });
+        $('.trumbowyg-box').focusin(function(event) {
+            $(this).find('.trumbowyg-button-pane').fadeIn(200);
+        });
 
-	    <!-- Custom scripts for this template -->
-	    <script src="../js/creative.min.js"></script>
-		<script src="../js/trumbowyg/trumbowyg.min.js"></script>
-		<script type="text/javascript">
-			$('#area').trumbowyg({
-				autogrow: true
-			});
+        function activateEditmode() {
+            let formData = new FormData();
+            formData.append("edit_key", editmode_key);
+            $.ajax({
+                type: 'POST',
+                url: 'admin_api.php',
+                data: formData
+            }).done(function(response) {
+                Cookies.set("editkey", response);
+                $('#editmodeActivator').after("<div class='alert alert-success'>Muutmine aktiveeritud. Lülitub välja brauseri sulgemisel.</div>");
+            }).fail(function(response) {
+                console.log(response);
+                $('#editmodeActivator').after("<div class='alert alert-danger'>Muutmise aktiveeritmine ebaõnnestus!</div>");
+            });
+        }
 
-			('.trumbowyg-button-pane').css('display','none');
-			$('.trumbowyg-box').focusout(function(event){
-				$(this).find('.trumbowyg-button-pane').fadeOut(200);
-			});
-			$('.trumbowyg-box').focusin(function(event){
-				$(this).find('.trumbowyg-button-pane').fadeIn(200);
-			});
+    </script>
+</body>
 
-		</script>
-	</body>
 </html>
