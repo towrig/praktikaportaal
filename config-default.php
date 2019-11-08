283fef70ed15ffb48a2ae66753d1ac38e49adc61
Kait Krull <kaitkrull@hotmail.com>
2019-11-08 09:34:00 -0500

    Added gitignore and config template

---
+++ config-default.php
@@ -0,0 +1,10 @@
+<?php
+
+$cfg = new stdClass();
+// NB! With trailing slash!
+$cfg->wwwroot   = 'http://example.com/';
+
+$cfg->dbhost 	  = 'localhost';
+$cfg->dbname 	  = 'dbname';
+$cfg->dbuser 	  = 'dbuser';
+$cfg->dbpasswd 	= 'dbpassword';
