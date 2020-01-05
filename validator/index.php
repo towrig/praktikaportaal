bce38ea9aa150466dd4c6f64702f458cb1717066
Kait Krull <kaitkrull@hotmail.com>
2020-01-04 23:51:52 -0500

    #2 Validator page design to match current theme

--- validator/index.php
+++ validator/index.php
@@ -56,8 +56,8 @@ $dbpassword = $CFG->dbpasswd;
 			}
 		}
 		$res = $query->execute(array($validationcode));
-		$heading = "Tehtud!";
-		$paragraph = "Sinu postitus on nüüd aktiveeritud!";
+		$heading = "Tehtud";
+		$paragraph = "Postitus aktiveeritud";
 	}
 	catch(PDOException $e){
 		$paragraph = "Ühendus ebaõnnestus: " . $e->getMessage();
@@ -67,81 +67,28 @@ $dbpassword = $CFG->dbpasswd;
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Praktikavahenduste keskkond</title>
-
-    <!-- Font Awesome Icons -->
-    <!--<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">-->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
-
-    <!-- Plugin CSS -->
-    <link href="../vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
-
-    <!-- Theme CSS - Includes Bootstrap -->
-    <link href="../css/creative.min.css" rel="stylesheet">
-
-    <!-- Theme CSS - Includes Bootstrap -->
-    <link href="../css/custom.css?v=2" rel="stylesheet">
-
-</head>
-<body>
-
-	<!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-        <div class="container">
-            <a class="navbar-brand js-scroll-trigger" href="../">Praktikavahenduste keskkond</a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="#about">Kuidas valida?</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="#services">Kuidas areneda?</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="#portfolio">Miks ...?</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="#contact">Kontakt</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-	
-	<div id="page-content">
-		
-		<section class="page-section bg-primary">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-12 text-center">
-                    <hr class="divider light my-4">
-                    <h2><?php echo $heading; ?></h2>
-                    <p><?php echo $paragraph; ?></p>
-                    <hr class="divider light my-4">
-
-                </div> <!-- .col-->
-                
+<?php $title="Postitus aktiveeritud!"; include_once './../templates/header.php';?>
+<body id="validated" class="validated">
+    <?php include_once './../templates/top-navbar.php';?>
+    <div id="main"></div>
+    <header id="masthead" class="masthead d-flex flex-wrap">
+      <div id="page-content" class="container">
+        <section class="page-section">
+          <div class="bg-container">
+            <div class="img-cont"></div>
+          </div>
+          <div class="container">
+            <div class="row text-uppercase">
+              <div class="col-lg-3 col-md-12">
+                <h2 class="mb-5"><span><?php echo $heading; ?></span><br><?php echo $paragraph; ?></h2>
+              </div>
+              <!-- .col-->
             </div> <!-- .row -->
-        </div> <!-- .container -->
-    </section>
-
-	</div>
-
-	
+          </div> <!-- .container -->
+        </section>
+      </div>
+    </header>
+  <!-- Footer -->
+  <?php include_once './../templates/footer.php';?>
 </body>
 </html>
