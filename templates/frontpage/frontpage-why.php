d7b786c3f5c161924bfdead257d506cc85b3e38b
Kait Krull <kaitkrull@hotmail.com>
2020-01-04 23:52:31 -0500

    #2 Frontpage why section cleanup + positioning

--- templates/frontpage/frontpage-why.php
+++ templates/frontpage/frontpage-why.php
@@ -1,10 +1,9 @@
-<section id="why" class="d-flex flex-wrap align-content-center mb-5">
+<section id="why" class="d-flex flex-wrap align-content-center mb-5 pt-5">
   <div class="container">
     <h2 class="mt-5 pt-5 text-center">Miks <span>FUTULAB?</span></h2>
     <small>fUTulab toetab Sinu tuleviku- ja karjäärivõimalusi ja lisaks veel ilusat ja kaunist teksti, mis aitab mõista, mida see plokk räägib</small>
     <div class="row text-white mb-5">
       <div id="why-vorgustik" class="why-box col-md-4">
-        <!--<i class="fas fa-4x fa-thumbs-up mt-3"></i>-->
         <div class="col p-3">
           <h4>Võrgustik</h4>
           <i id="why--vorgustik"></i>
@@ -12,7 +11,6 @@
         </div>
       </div>
       <div id="why-konkurents" class="why-box col-md-4">
-        <!--<i class="fas fa-4x fa-retweet mt-3"></i>-->
         <div class="col p-3 my-3 my-md-5">
           <h4>Konkurents</h4>
           <i id="why--konkurents"></i>
@@ -20,7 +18,6 @@
         </div>
       </div>
       <div id="why-areng" class="why-box col-md-4">
-        <!--<i class="fas fa-4x fa-check mt-3"></i>-->
         <div class="col p-3">
           <h4>Areng</h4>
           <i id="why--areng"></i>
