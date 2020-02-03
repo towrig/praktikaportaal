13ae59d3ccd978867de8d2a5e3a0f6f7490ca5d0
Kait Krull <kaitkrull@hotmail.com>
2020-02-03 01:59:31 -0500

    #2 Unique page - Removed pesky dots and force "-" on longer words

--- templates/frontpage/frontpage-unique.php
+++ templates/frontpage/frontpage-unique.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h2 data-aos="fade-down">Mis teeb Futulabi <span>ainulaadseks?</span></h2>
-                <small data-aos="fade-down">Pakume tulevikku vaatavaid ja osaliste arengut soodustavaid koostöövõimalusi.</small>
+                <small data-aos="fade-down">Pakume tulevikku vaatavaid ja osaliste arengut soodustavaid koostöövõimalusi</small>
             </div>
         </div>
     </div>
@@ -18,23 +18,23 @@
       </div>
       <div id="unique-interditsiplinaarne" class="unique-box col-lg-3 col-md-6"  data-aos="flip-down" data-aos-anchor-placement="bottom-bottom">
         <div class="col p-3">
-          <h3>Erialade&shy;vahelisus</h3>
+          <h3>Erialade-<br>vahelisus</h3>
           <i class="unique-arrow"></i>
           <span>Sa saad tegutseda keskkonnas, kus nii ülesanded kui ka meeskonnad ületavad eriala piire, leida uusi tuttavaid ning õppida nägema ja mõistma probleemide eri tahke</span>
         </div>
       </div>
       <div id="unique-tulevikuoskused" class="unique-box col-lg-3 col-md-6"  data-aos="flip-down" data-aos-anchor-placement="bottom-bottom">
         <div class="col p-3">
-          <h3>Tuleviku&shy;oskused</h3>
+          <h3>Tuleviku-<br>oskused</h3>
           <i class="unique-arrow"></i>
-          <span>Tulevikuülesanded nõuavad tulevikuoskuste arendamist. Need on Sinu edu alus ka kümne aasta pärast.</span>
+          <span>Tulevikuülesanded nõuavad tulevikuoskuste arendamist. Need on Sinu edu alus ka kümne aasta pärast</span>
         </div>
       </div>
         <div id="unique-paindlik" class="unique-box col-lg-3 col-md-6"  data-aos="flip-down" data-aos-anchor-placement="bottom-bottom">
         <div class="col p-3">
           <h3>Paindlikkus</h3>
           <i class="unique-arrow"></i>
-          <span>Sa võid ise otsustada, millal, kus ja kuidas osaled, sest nii piirid kui ka võimalused sead endale ise.</span>
+          <span>Sa võid ise otsustada, millal, kus ja kuidas osaled, sest nii piirid kui ka võimalused sead endale ise</span>
         </div>
       </div>
     </div>
