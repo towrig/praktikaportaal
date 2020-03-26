5b51575001316edc45be92cfc52b63480699a50a
Myseolumic <karl32123@gmail.com>
2020-03-26 13:56:19 +0200

    bugfix

--- uliopilased/index.php
+++ uliopilased/index.php
@@ -4,9 +4,9 @@
   $t_pieces = t(array("uliop_desc"));
   $title="Üliõpilased |";
   /*$description = "Otsid praktikakohta? Lisa oma profiil ja ole tööandjatele nähtav! Praktikakogemus suurendab edu edaspidisel töölekandideerimisel ja ka praktikakohast võib saada Sinu järgmine töökoht.";*/
-  $description = $t_pieces["uliop_desc"];
   include_once './../templates/header.php';
   include_once './../functions.php';
+  $description = $t_pieces["uliop_desc"];
 ?>
 
 <body id="page-top" class="practice">
